b8623b6ca392968f8e29c838f287301a68bf4af6
Charly <charly5bollinger@gmail.com>
2021-05-06 16:58:29 +0200

    Fixed smooth diff
    Nodes were inverted

--- AppMeshPOM/Source/op_smooth_difference.cpp
+++ AppMeshPOM/Source/op_smooth_difference.cpp
@@ -3,8 +3,8 @@
 
 double OpSmoothDifference::Signed(const Vector& vec) const
 {
-	double a = left->Signed(vec);
-	double b = right->Signed(vec);
+	double b = left->Signed(vec);
+	double a = right->Signed(vec);
 	double h = Math::Clamp(0.5 - 0.5 * (b + a) / k);
 	return Math::Mix(b, -a, h) + k * h * (1.0 - h);
 }
